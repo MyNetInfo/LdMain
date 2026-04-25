600514409dca3692c8e03d3b69ac69c9eecb65f8
JornLee <sbpfikdm@gmail.com>
2026-04-25 20:20:27 +0800

    ??

---
+++ temp/设置Svn代理.sh
@@ -0,0 +1,8 @@
+
+~/.subversion/config
+
+[global]
+http-proxy-port = 6600
+http-proxy-host = 10.6.6.102
+http-proxy-username = your-username # 可选，如果代理需要认证
+http-proxy-password = your-password # 可选，如果代理需要认证
