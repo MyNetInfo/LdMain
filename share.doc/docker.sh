9d90e245494bf09f81d47d99ccf8ed68eae549a2
JornLee <sbpfikdm@gmail.com>
2026-05-14 09:07:30 +0800

    ??

--- share.doc/docker.sh
+++ share.doc/docker.sh
@@ -3,6 +3,8 @@
 容器状态 sudo docker stats
 容器列表 sudo docker ps -a --format '{{.Names}} {{.Status}} {{.Ports}}' | sort | column -t
 
+删除容器 sudo docker rm -f Nginx
+
 重启容器 sudo docker restart Nginx
 登录容器 sudo docker exec -it Nginx /bin/bash
 查看日志 sudo docker logs -f Nginx --tail=20
